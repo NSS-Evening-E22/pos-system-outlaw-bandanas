47c9fe04a1e6600df2470591f6823097648203de
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-03-15 19:23:16 -0400

    created homescreen that welcomes user and gives them buttons to press. Clicking the logo will take you back to the home page. All buttons are funcitonal

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,5 +1,22 @@
+import renderCreateItemPage from '../pages/createItemPage';
+import renderViewOrdersPage from '../pages/viewOrdersPage';
+import renderRevenuePage from '../pages/revenuePage';
+
 const domEvents = () => {
-  console.warn('domEvents');
+  document.querySelector('#app').addEventListener('click', (e) => {
+    console.warn(e.target.id);
+    if (e.target.id.includes('view-orders')) {
+      renderViewOrdersPage();
+    }
+
+    if (e.target.id.includes('create-order')) {
+      renderCreateItemPage();
+    }
+
+    if (e.target.id.includes('view-revenue')) {
+      renderRevenuePage();
+    }
+  });
 };
 
 export default domEvents;
